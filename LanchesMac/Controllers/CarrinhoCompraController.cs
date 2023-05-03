b9243a86a64ffb6dccb595398f8082b53675624b
petinari <petinari@outlook.com>
2023-05-02 21:25:14 -0400

    02052023

---
+++ LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers;
+
+public class CarrinhoCompraController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ LanchesMac/Views/CarrinhoCompra/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LanchesMac.Views.CarrinhoCompra;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
